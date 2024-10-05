38eef117cf7b17d945743237a6081f796ad77e41
ignacioj049 <igjara2022@inf.udec.cl>
2024-10-04 22:32:10 -0300

    Prototipo Final

---
+++ src/Exception.java
@@ -0,0 +1,17 @@
+ class PagoIncorrectoException extends Exception {
+    public PagoIncorrectoException(String message) {
+        super(message);
+    }
+}
+
+ class NoHayProductoException extends Exception {
+    public NoHayProductoException(String message) {
+        super(message);
+    }
+}
+
+ class PagoInsuficienteException extends Exception {
+    public PagoInsuficienteException(String message) {
+        super(message);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            Expendedor expendedor = new Expendedor(5); // Sin precio fijo
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            Comprador comprador1 = new Comprador(new Moneda.Moneda1000(), 0, expendedor);
+            System.out.println("Comprador 1 bebió: " + comprador1.queBebiste());
+            System.out.println("Comprador 1 tiene de vuelto: " + comprador1.cuantoVuelto());
+
+            Comprador comprador2 = new Comprador(new Moneda.Moneda500(), 1, expendedor);
+            System.out.println("Comprador 2 bebió: " + comprador2.queBebiste());
+            System.out.println("Comprador 2 tiene de vuelto: " + comprador2.cuantoVuelto());
+
+            // Compras de dulces
+            Comprador comprador3 = new Comprador(new Moneda.Moneda1000(), 3, expendedor);
+            System.out.println("Comprador 3 compró: " + comprador3.queBebiste());
+            System.out.println("Comprador 3 tiene de vuelto: " + comprador3.cuantoVuelto());
+
+            Comprador comprador4 = new Comprador(new Moneda.Moneda100(), 4, expendedor);
+            System.out.println("Comprador 4 compró: " + comprador4.queBebiste());
+            System.out.println("Comprador 4 tiene de vuelto: " + comprador4.cuantoVuelto());
+
+            Comprador comprador5 = new Comprador(null, 2, expendedor);
+        } catch (PagoIncorrectoException e) {
+            System.out.println(e.getMessage());
+        } catch (NoHayProductoException | PagoInsuficienteException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
